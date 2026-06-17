066900244417a0638ad0eb54e23fda6b5af5874d
Diego Serra <serradiego2005@icloud.com>
2026-06-17 18:32:21 +0200

    Add Taverna objects
    Include two new Taverna object sources in the project and perform a large overhaul of the generated XML documentation. Updated ECARules4All-DLL.csproj to compile Taxonomies/Objects/Taverna/ECAAirPurifier.cs and ECAFireplace.cs (new files added). Extensive edits to ECARules4All_DLL.xml: standardized quote usage, clarified and expanded summaries for many behaviours and object types, added/renamed taxonomy entries (e.g. Collectable, Container, Counter, Highlight, Keypad, Lock, Particle, Placeholder, Switch, Timer, Transition, Trigger, ECALiquidDrop/ECALiquidSpawner and numerous animal subtypes), and improved member/method descriptions and parameter documentation for better API clarity.

---
+++ Taxonomies/Objects/Taverna/ECAAirPurifier.cs
@@ -0,0 +1,65 @@
+﻿using ECARules4All_DLL.Taxonomies.Objects.Interactions;
+using ECARules4All_DLL.Utils;
+using UnityEngine;
+
+namespace ECARules4All_DLL.Taxonomies.Objects.Taverna
+{
+    [ECARules4All("airpurifier")]
+    [RequireComponent(typeof(ECAInteraction))]
+    [DisallowMultipleComponent]
+    public class ECAAirPurifier : ECAObject
+    {
+        [ECARelevance(true)]
+        [StateVariable("isPoweredOn", ECARules4AllType.Boolean)]
+        public ECABoolean isPoweredOn
+        {
+            get => _isPoweredOn;
+            set
+            {
+                _isPoweredOn = value;
+                ECAScript.NotifyUpdate(this, nameof(isPoweredOn), isPoweredOn.ToString());
+            }
+        }
+        [SerializeField]
+        private ECABoolean _isPoweredOn = new ECABoolean(ECABoolean.BoolType.OFF);
+
+        [StateVariable("fanSpeed", ECARules4AllType.Integer)]
+        public int fanSpeed
+        {
+            get => _fanSpeed;
+            set
+            {
+                _fanSpeed = value;
+                ECAScript.NotifyUpdate(this, nameof(fanSpeed), fanSpeed.ToString());
+            }
+        }
+        [SerializeField]
+        private int _fanSpeed = 0;
+
+        /// Attiva il purificatore d'aria.
+        [ECARelevance(true)]
+        [Action(typeof(ECAAirPurifier), "turnOn")]
+        public void TurnOn()
+        {
+            isPoweredOn = new ECABoolean(ECABoolean.BoolType.ON);
+            fanSpeed = fanSpeed==0? 1: fanSpeed; // Imposta una velocità minima di avvio se era a 0
+        }
+
+        /// Spegne il purificatore d'aria e azzera la velocità della ventilatore
+        [ECARelevance(true)]
+        [Action(typeof(ECAAirPurifier), "turnOff")]
+        public void TurnOff()
+        {
+            isPoweredOn = new ECABoolean(ECABoolean.BoolType.OFF);
+            fanSpeed = 0;
+        }
+        
+        /// Imposta una velocità specifica per la ventola.
+        [Action(typeof(ECAAirPurifier), "sets", "speed", "to", typeof(int))]
+        public void SetSpeed(int speed)
+        {
+            fanSpeed = speed;
+            isPoweredOn = new ECABoolean(fanSpeed > 0 ? ECABoolean.BoolType.ON : ECABoolean.BoolType.OFF);
+        }
+    }
+}
---
+++ Taxonomies/Objects/Taverna/ECAFireplace.cs
@@ -0,0 +1,123 @@
+﻿using ECARules4All_DLL.Taxonomies.Objects.Interactions;
+using ECARules4All_DLL.Taxonomies.Objects.Interactions.Subcategories;
+using ECARules4All_DLL.Utils;
+using UnityEngine;
+
+namespace ECARules4All_DLL.Taxonomies.Objects.Taverna
+{
+    [ECARules4All("fireplace")]
+    [RequireComponent(typeof(ECAInteraction))]
+    [DisallowMultipleComponent]
+    public class ECAFireplace : ECAObject
+    {
+        [Header("Fireplace Properties (HASS)")]
+        [ECARelevance(true)]
+        [StateVariable("isLit", ECARules4AllType.Boolean)]
+        public ECABoolean isLit
+        {
+            get => _isLit;
+            set
+            {
+                _isLit = value;
+                ECAScript.NotifyUpdate(this, nameof(isLit), isLit.ToString());
+            }
+        }
+        [SerializeField]
+        private ECABoolean _isLit = new ECABoolean(ECABoolean.BoolType.OFF);
+
+        [StateVariable("intensity", ECARules4AllType.Integer)]
+        public int intensity
+        {
+            get => _intensity;
+            set
+            {
+                _intensity = value;
+                ECAScript.NotifyUpdate(this, nameof(intensity), intensity.ToString());
+                UpdateVisualEffects();
+                SyncWithLinkedLight();
+            }
+        }
+        [SerializeField]
+        private int _intensity = 0;
+
+        [StateVariable("smokeLevel", ECARules4AllType.Float)]
+        public float smokeLevel
+        {
+            get => _smokeLevel;
+            set
+            {
+                _smokeLevel = value;
+                ECAScript.NotifyUpdate(this, nameof(smokeLevel), smokeLevel.ToString());
+            }
+        }
+        [SerializeField]
+        private float _smokeLevel = 0f;
+
+        [Header("Visual Effects Components")]
+        public ParticleSystem smokeParticles;
+        
+        private ECALight linkedEcaLight;
+
+        protected void Awake()
+        {
+            linkedEcaLight = GetComponentInChildren<ECALight>(); 
+            UpdateVisualEffects();
+        }
+
+        /// Azione per accendere il camino (Imposta intensità a 20 di default)
+        [ECARelevance(true)]
+        [Action(typeof(ECAFireplace), "ignite")]
+        [ContextMenu("Ignite")]
+        public void Ignite()
+        {
+            isLit = new ECABoolean(ECABoolean.BoolType.ON);
+            intensity = 20;
+            smokeLevel = 33.3f;
+        }
+
+        /// Azione per spegnere completamente il camino
+        [ECARelevance(true)]
+        [Action(typeof(ECAFireplace), "extinguish")]
+        [ContextMenu("Extinguish")]
+        public void Extinguish()
+        {
+            isLit = new ECABoolean(ECABoolean.BoolType.OFF);
+            intensity = 0;
+            smokeLevel = 0f;
+        }
+
+        /// Azione per impostare l'intensità del camino
+        [Action(typeof(ECAFireplace), "sets", "intensity", "to", typeof(int))]
+        public void SetIntensity(int level)
+        {
+            isLit = new ECABoolean(level > 0 ? ECABoolean.BoolType.ON : ECABoolean.BoolType.OFF);
+            intensity = level;
+            smokeLevel = level * 33.3f;
+        }
+        
+        private void UpdateVisualEffects()
+        {
+            if (smokeParticles == null) return;
+
+            if (intensity > 0)
+            {
+                if (!smokeParticles.isPlaying) smokeParticles.Play();
+                
+                var emission = smokeParticles.emission;
+                emission.rateOverTime = intensity * 2f;
+            }
+            else
+            {
+                if (smokeParticles.isPlaying) smokeParticles.Stop();
+            }
+        }
+
+        private void SyncWithLinkedLight()
+        {
+            if (linkedEcaLight == null) return;
+
+            linkedEcaLight.Turns(isLit);
+            linkedEcaLight.SetsIntensity(intensity);
+        }
+    }
+}
